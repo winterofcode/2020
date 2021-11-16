85e019dd2f62e6df7d3309fc26cdf9fe9503609c
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-11-16 20:54:31 +0530

    Updates

---
+++ precache-manifest.f73d89160bfca1461b5590d6bc97b1e8.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cb5e0fd70a466788e9c8264e8849bf",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "2fb36498e20becf4fd50",
+    "url": "/2020/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "c5eb075b779a83668d1d",
+    "url": "/2020/static/css/main.ea2e1165.chunk.css"
+  },
+  {
+    "revision": "2fb36498e20becf4fd50",
+    "url": "/2020/static/js/2.bec88868.chunk.js"
+  },
+  {
+    "revision": "2e2f93f9083d86424e4830354502fb47",
+    "url": "/2020/static/js/2.bec88868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5eb075b779a83668d1d",
+    "url": "/2020/static/js/main.03b133a6.chunk.js"
+  },
+  {
+    "revision": "0c8dc197aec9e80ec722",
+    "url": "/2020/static/js/runtime-main.58d794ce.js"
+  },
+  {
+    "revision": "22f32297f8f18b10f26dcb8c2f79ff03",
+    "url": "/2020/static/media/Abel-Mathew.22f32297.jpg"
+  },
+  {
+    "revision": "9c22b54dee9169fff715a77afe585e7e",
+    "url": "/2020/static/media/Abhibhaw-Kumar-Asthana.9c22b54d.jpeg"
+  },
+  {
+    "revision": "ab2365935f871d622dd0340bb9141c1e",
+    "url": "/2020/static/media/Abhijit-Tripathy.ab236593.jpeg"
+  },
+  {
+    "revision": "c04e2a78a6b77bdf1923721e4a786516",
+    "url": "/2020/static/media/Abhishek-Nandy.c04e2a78.jpg"
+  },
+  {
+    "revision": "81119c3e0db86043982cdb34c79a8de1",
+    "url": "/2020/static/media/Alienbrains.81119c3e.png"
+  },
+  {
+    "revision": "3fa1a31ce36b3b78468a3d0687a801e7",
+    "url": "/2020/static/media/Aniket-Kumar.3fa1a31c.jpg"
+  },
+  {
+    "revision": "9e944f2f9509e2337c8acbbba7f75ac7",
+    "url": "/2020/static/media/Anjali-Jha.9e944f2f.jpeg"
+  },
+  {
+    "revision": "f1e4ec1fd52b26299b5f392d95fc3d95",
+    "url": "/2020/static/media/Ankesh-Anku.jpg.f1e4ec1f.jpeg"
+  },
+  {
+    "revision": "defeb1d0e9ca07f8e7c037367452d396",
+    "url": "/2020/static/media/Ankit-Aggarwal.defeb1d0.jpg"
+  },
+  {
+    "revision": "fce985134482c12388270e3087ce1b49",
+    "url": "/2020/static/media/Anubhav-Singh.fce98513.jpeg"
+  },
+  {
+    "revision": "228a1ad62195b3fda8bae480c3dd581c",
+    "url": "/2020/static/media/Anuska-Sinha.228a1ad6.jpeg"
+  },
+  {
+    "revision": "4fec2f04b25c6b7ff0111ec3f360655d",
+    "url": "/2020/static/media/Aritra Sen.4fec2f04.jpg"
+  },
+  {
+    "revision": "3190632e16b10d2e34fb659f045a8da1",
+    "url": "/2020/static/media/Ashutosh-Krishna.3190632e.jpg"
+  },
+  {
+    "revision": "f08c85c09fa4b972229594c99166c68b",
+    "url": "/2020/static/media/Ayush-Singh.f08c85c0.jpg"
+  },
+  {
+    "revision": "725611e8824492db2ca5fbd2f16723a3",
+    "url": "/2020/static/media/Bala-Priya-C.725611e8.png"
+  },
+  {
+    "revision": "bdc020462e3395b6ff5bce9cee90dd2c",
+    "url": "/2020/static/media/Chinmay-Kabi.bdc02046.jpeg"
+  },
+  {
+    "revision": "f47a5aa8c32188f2fc9e267ae570d57d",
+    "url": "/2020/static/media/Debjeet-Banerjee.f47a5aa8.jpeg"
+  },
+  {
+    "revision": "41c90427d94d393b28e0a9e841c21ec4",
+    "url": "/2020/static/media/Deeptendu-Santra.41c90427.jpeg"
+  },
+  {
+    "revision": "840c8290dbfed2fc1de4da598d4a849a",
+    "url": "/2020/static/media/Dheeraj-Kotwani.840c8290.jpeg"
+  },
+  {
+    "revision": "83c6ae1fef9154efed0cae37d1337aeb",
+    "url": "/2020/static/media/Divya-Prakash.83c6ae1f.jpeg"
+  },
+  {
+    "revision": "8fa658b19519f71c8f1174d09fafedf8",
+    "url": "/2020/static/media/Diya-Jaiswal.8fa658b1.jpeg"
+  },
+  {
+    "revision": "cd14f8975da5889eb96849df1f75bbd9",
+    "url": "/2020/static/media/Farhan-Hai-Khan.cd14f897.jpg"
+  },
+  {
+    "revision": "a258cc9add003728eb02b4a9da2f71fe",
+    "url": "/2020/static/media/Google Developers.a258cc9a.svg"
+  },
+  {
+    "revision": "a88c84d90045e6254f743ed74c8768d7",
+    "url": "/2020/static/media/Joydeep-Das.a88c84d9.jpg"
+  },
+  {
+    "revision": "e6010dc68d952bbdeef79bf12ea14f0d",
+    "url": "/2020/static/media/Kajol-Kumari.e6010dc6.jpg"
+  },
+  {
+    "revision": "6370e75cb903c5ec46e60eaf22d9f8cf",
+    "url": "/2020/static/media/Mahak-Makharia.6370e75c.jpeg"
+  },
+  {
+    "revision": "360d1461a2b68062a0fece7848eabefe",
+    "url": "/2020/static/media/Manning.360d1461.svg"
+  },
+  {
+    "revision": "f710fe13d2a16b0ef848f543a890dcc4",
+    "url": "/2020/static/media/Manshi-Todi.f710fe13.jpeg"
+  },
+  {
+    "revision": "ca2fb64330a4412a77858a43e402f350",
+    "url": "/2020/static/media/Patibanda-Sriram.ca2fb643.jpg"
+  },
+  {
+    "revision": "c44e5ca08f3fc6046bdf663a5d1cf600",
+    "url": "/2020/static/media/Pratyay-Banerjee.c44e5ca0.jpg"
+  },
+  {
+    "revision": "528ac17a04e620f9a1e25ccd6a8c3720",
+    "url": "/2020/static/media/Qazi-Haque.528ac17a.jpg"
+  },
+  {
+    "revision": "b00a5bdc899d00fc4c856d9977c89102",
+    "url": "/2020/static/media/Qwiklabs.b00a5bdc.png"
+  },
+  {
+    "revision": "d47697b953f900e8a5e7e3b97a464f03",
+    "url": "/2020/static/media/Rajwrita-Nath.d47697b9.jpg"
+  },
+  {
+    "revision": "e6fe7834c292b32006ea732f65d88208",
+    "url": "/2020/static/media/Rishiraj-Acharya.e6fe7834.jpg"
+  },
+  {
+    "revision": "3a69221a1aeb2fd0b4bdad23a49bdc97",
+    "url": "/2020/static/media/Ritesh-Patil.3a69221a.jpeg"
+  },
+  {
+    "revision": "6c85d51765e16097232a204e12948038",
+    "url": "/2020/static/media/Ritesh.6c85d517.jpg"
+  },
+  {
+    "revision": "52aa2a4191b37fd3cead7152fd4cf245",
+    "url": "/2020/static/media/Saikiran-Reddy.52aa2a41.png"
+  },
+  {
+    "revision": "79e563febd027f1ae25534a2fb4329a0",
+    "url": "/2020/static/media/Saurav-Kumar.79e563fe.jpg"
+  },
+  {
+    "revision": "fd7f80e8bb2da2ba78ed0eab5c760f04",
+    "url": "/2020/static/media/Sayak-Paul.fd7f80e8.jpg"
+  },
+  {
+    "revision": "00e8e8a1b409db868d9e3a8deb2361ba",
+    "url": "/2020/static/media/Shailly-Keshari.00e8e8a1.jpg"
+  },
+  {
+    "revision": "d60e3d33c8be6d04b522f9668eda30e1",
+    "url": "/2020/static/media/Shraddha-Prasad.d60e3d33.jpg"
+  },
+  {
+    "revision": "ef605dc946e29de68354a1eb9ef2345e",
+    "url": "/2020/static/media/Shubhank-Saxena.ef605dc9.jpeg"
+  },
+  {
+    "revision": "00898e87316acafeaddeec0e64354141",
+    "url": "/2020/static/media/Smarak-Das.00898e87.jpg"
+  },
+  {
+    "revision": "7608f049c62cf0c1ac5b1d2a6eecd640",
+    "url": "/2020/static/media/Sucheta-Nandy.7608f049.jpg"
+  },
+  {
+    "revision": "3b35b165e0c800b0e2c5738dae1a98bb",
+    "url": "/2020/static/media/Tannistha-Pal.3b35b165.jpg"
+  },
+  {
+    "revision": "75d2f89565ade8975a539bb96d34601d",
+    "url": "/2020/static/media/Vanshika-Garg.75d2f895.jpg"
+  },
+  {
+    "revision": "96efffbd6e2e1d6c11dcfed53db31fa2",
+    "url": "/2020/static/media/Yashaswi-Biyahut.96efffbd.jpeg"
+  },
+  {
+    "revision": "94c98790858b01a28943cd371c11a79c",
+    "url": "/2020/static/media/abhinav-anand.94c98790.jpg"
+  },
+  {
+    "revision": "ed3feeb103c78a9c533a0b64b4405257",
+    "url": "/2020/static/media/absurdNerd.ed3feeb1.png"
+  },
+  {
+    "revision": "d109ce70816422873c9e99251e778694",
+    "url": "/2020/static/media/balaka_biswas.d109ce70.jpeg"
+  },
+  {
+    "revision": "2bdf6195cdfbdc401685b6bf119b2c96",
+    "url": "/2020/static/media/canvasbird.2bdf6195.png"
+  },
+  {
+    "revision": "25f6f8537c3a10a7100f14a1f228dd59",
+    "url": "/2020/static/media/deepfusionai.25f6f853.png"
+  },
+  {
+    "revision": "e7898e89c007809465fc015e5f020c82",
+    "url": "/2020/static/media/dsciem.e7898e89.png"
+  },
+  {
+    "revision": "d5dc0bad221f7ff9f5438a25945379af",
+    "url": "/2020/static/media/dscnitrourkela.d5dc0bad.png"
+  },
+  {
+    "revision": "69c2ebf39dd78cb6028a3d5808001444",
+    "url": "/2020/static/media/dscnsec.69c2ebf3.png"
+  },
+  {
+    "revision": "25d12e0048c5d84251e925cf2fc255c7",
+    "url": "/2020/static/media/dscx.25d12e00.png"
+  },
+  {
+    "revision": "570cefe305243f1c2c65f0a12141a9dc",
+    "url": "/2020/static/media/eduAlgo.570cefe3.png"
+  },
+  {
+    "revision": "117e0db9ec0994169ff08db81c27d453",
+    "url": "/2020/static/media/fireshort.117e0db9.png"
+  },
+  {
+    "revision": "407dfe4854d18e2b8d862f051488bc8c",
+    "url": "/2020/static/media/goli-akshay.407dfe48.jpg"
+  },
+  {
+    "revision": "0feb1276f2f798694f106f16b469ead0",
+    "url": "/2020/static/media/how.0feb1276.png"
+  },
+  {
+    "revision": "c2a7e81c7dc8483d533de337b3b3e80b",
+    "url": "/2020/static/media/kiwismedia.c2a7e81c.png"
+  },
+  {
+    "revision": "fcb58d7dd64ab34a4ac820b4307f7f14",
+    "url": "/2020/static/media/latter.fcb58d7d.png"
+  },
+  {
+    "revision": "935d7f48cf9e2f95308d573f27a556ea",
+    "url": "/2020/static/media/org-logo.935d7f48.png"
+  },
+  {
+    "revision": "36ff8f9a0feb08379cc628c255c88b76",
+    "url": "/2020/static/media/soham-biswas.36ff8f9a.jpeg"
+  },
+  {
+    "revision": "f57cbe121f592e2f1c0768d230c7e0cc",
+    "url": "/2020/static/media/swags.f57cbe12.png"
+  },
+  {
+    "revision": "e7d6a11246e7436861d5493c1ecc39f9",
+    "url": "/2020/static/media/tesseract.e7d6a112.png"
+  },
+  {
+    "revision": "901e3c04c126b7c435a8bfd2db84abe1",
+    "url": "/2020/static/media/vatsal-kesarwani.901e3c04.jpg"
+  },
+  {
+    "revision": "124e1ccde7714a86f6b032b3d1b65e66",
+    "url": "/2020/static/media/vishwajeet.124e1ccd.png"
+  }
+]);
